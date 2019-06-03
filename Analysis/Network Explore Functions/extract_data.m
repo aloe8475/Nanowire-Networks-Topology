f26b4b64ade02cf61e1c3d0e311aade822803300
aloe8475 <50392552+aloe8475@users.noreply.github.com>
2019-06-03 16:33:19 +1000

    Automated to allow for more than 2 electrodes

--- Analysis/Network Explore Functions/extract_data.m
+++ Analysis/Network Explore Functions/extract_data.m
@@ -1,11 +1,24 @@
 function extract_data(network,simNum)
 %Training Data:
-IDrain1=network.Simulations{simNum}.Data.IDrain1;
-IDrain2=network.Simulations{simNum}.Data.IDrain2;
-VSource1=network.Simulations{simNum}.Data.VSource1;
-VSource2=network.Simulations{simNum}.Data.VSource2;
+numElec=height(network.Simulations{simNum}.Electrodes);
 
-path='D:\alon_\Research\POSTGRAD\PhD\CODE\Adrian''s Code\NETWORK_sims_2\Saved Networks\Simulations Only\Python Data\';
+% IDrain1=network.Simulations{simNum}.Data.IDrain1;
+% VSource1=network.Simulations{simNum}.Data.VSource1;
+drainIndex=find(contains(network.Simulations{simNum}.Electrodes.Name,'Drain'));
+sourceIndex=find(contains(network.Simulations{simNum}.Electrodes.Name,'Source'));
 
-save([path network.Simulations{simNum}.Type '_' num2str(simNum) '_ForPython.mat'],'IDrain1','IDrain2','VSource1','VSource2');
+for i = 1:length(sourceIndex)
+VSource(:,i)=network.Simulations{simNum}.Data.(['VSource' num2str(i)]);
+end 
+for i=1:length(drainIndex)
+    IDrain(:,i)=network.Simulations{simNum}.Data.(['IDrain' num2str(i)]);
+end 
+
+computer=getenv('computername');
+switch computer
+    case 'W4PT80T2'
+        path='C:\Users\aloe8475\Documents\GitHub\CODE\Adrian''s Code\NETWORK_sims_2\Saved Networks\Simulations Only\Python Data';
+% path='D:\alon_\Research\POSTGRAD\PhD\CODE\Adrian''s Code\NETWORK_sims_2\Saved Networks\Simulations Only\Python Data\';
+end 
+save([path network.Simulations{simNum}.Type '_' num2str(simNum) '_ForPython.mat'],'IDrain','VSource');
 end 
