381098ed4f0d26a08a9ed8d1580ffa6b691e043c
aloe8475 <50392552+aloe8475@users.noreply.github.com>
2020-03-12 16:05:54 +1100

    if anything f**cks up, revert to this

--- Analysis/Functional Connectivity/Functional Tasks/smallworldness.m
+++ Analysis/Functional Connectivity/Functional Tasks/smallworldness.m
@@ -1,10 +1,15 @@
 load('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Analysis\Functional Connectivity\Functional Tasks\300nwASN.mat')
 load('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Data\Organic Networks Connectomes\celegans277neurons.mat')
+load('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Analysis\Functional Connectivity\Functional Tasks\300_WS.mat')
+
 for i=1:length(AdjMat)
 smallworld(i)=small_world_propensity(double(AdjMat{i}));
+smallworld_grid(i)=small_world_propensity(double(AdjMat_Grid{i}));
+smallworld_random(i)=small_world_propensity(double(AdjMat_Random{i}));
 end
 cElegansSW=small_world_propensity(celegans277matrix);
 
 % save('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Analysis\Functional Connectivity\Functional Tasks\300nwASN_smallworld.mat','smallworld')
-save('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Analysis\Functional Connectivity\Functional Tasks\cElegans_smallworld.mat','cElegansSW')
+%save('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Analysis\Functional Connectivity\Functional Tasks\cElegans_smallworld.mat','cElegansSW')
+% save('C:\Users\aloe8475\Documents\PhD\GitHub\CODE\Analysis\Functional Connectivity\Functional Tasks\300_WS_smallworld.mat','smallworld_grid','smallworld_random')
 
